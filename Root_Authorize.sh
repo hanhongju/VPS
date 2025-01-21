fd8213fa4cc4931d509daab9a218430514f58385
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-01-21 12:33:53 +0800

    Update Root_Authorize.sh

--- Root_Authorize.sh
+++ Root_Authorize.sh
@@ -14,6 +14,17 @@ systemctl     restart     sshd
 
 
 
+installpubkey ()   {
+mkdir         /root/.ssh/
+echo    '
+ssh-ed25519   AAAAC3NzaC1lZDI1NTE5AAAAII645EjCCRKn2xs9mpL2HiiLAQYKHOA+nyESQ0qf3VBR
+'       >     /root/.ssh/authorized_keys
+systemctl     restart     sshd
+}
+
+
+
+
 
 # 更改root密码，安装公钥，开启ROOT密码登录
 # 添加多个公钥必须在authorized_keys中另加一行
