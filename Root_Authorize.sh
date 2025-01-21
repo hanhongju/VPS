04074542408dc6f3ee355482735e255d73acf6b3
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-01-21 12:34:26 +0800

    Update Root_Authorize.sh

--- Root_Authorize.sh
+++ Root_Authorize.sh
@@ -5,10 +5,6 @@ echo    '
 PermitRootLogin yes
 PasswordAuthentication yes
 '       >     /etc/ssh/sshd_config.d/convenient.conf
-mkdir         /root/.ssh/
-echo    '
-ssh-ed25519   AAAAC3NzaC1lZDI1NTE5AAAAII645EjCCRKn2xs9mpL2HiiLAQYKHOA+nyESQ0qf3VBR
-'       >     /root/.ssh/authorized_keys
 systemctl     restart     sshd
 
 
