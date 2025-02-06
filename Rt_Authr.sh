c9a7e09c7e75bc77b6f284c98713bb8c62cd2717
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-02-06 09:51:56 +0800

    Update Rt_Authr.sh

--- Rt_Authr.sh
+++ Rt_Authr.sh
@@ -5,6 +5,9 @@ echo    '
 PermitRootLogin yes
 PasswordAuthentication yes
 '       >>    /etc/ssh/sshd_config
+echo    '
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII645EjCCRKn2xs9mpL2HiiLAQYKHOA+nyESQ0qf3VBR
+'       >     /root/.ssh/authorized_keys
 systemctl     restart     sshd
 
 
