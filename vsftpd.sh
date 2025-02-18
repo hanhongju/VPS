dd76ea5092e7a68f79ef5e448168b43c10980ab3
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-02-18 09:52:37 +0800

    Update vsftpd.sh

--- vsftpd.sh
+++ vsftpd.sh
@@ -1,14 +1,19 @@
 site=8.138.172.34
-apt         -y        update    
-apt         -y        install      vsftpd net-tools
-ipv4=$(ping -c 2 $site | head -2 | tail -1 | awk '{print $5}' | sed 's/[(:)]//g')
-echo        $ipv4
-echo                  "pasv_address=$ipv4"             >>            /etc/vsftpd.conf
-sed         -i        's/^#\(write_enable=\)/\1/g'                   /etc/vsftpd.conf
-sed         -i        's/\(listen=\).*/\1YES/g'                      /etc/vsftpd.conf
-sed         -i        's/listen_ipv6=.*/listen_ipv6=NO/g'            /etc/vsftpd.conf
-useradd     -m        hhj      -d      /home/hhj/
-echo        -e        "hhj\nhhj"           |             passwd  hhj
+apt        -y      update    
+apt        -y      install                 vsftpd net-tools
+#ipv4=$(ping -c 2 $site | head -2 | tail -1 | awk '{print $5}' | sed 's/[(:)]//g')
+sed        -i      '/write_enable=/d'      /etc/vsftpd.conf
+sed        -i      '/listen=/d'            /etc/vsftpd.conf
+sed        -i      '/listen_ipv6=/d'       /etc/vsftpd.conf
+sed        -i      '/pasv_address=/d'      /etc/vsftpd.conf
+echo       "
+write_enable=YES
+listen=YES
+listen_ipv6=NO
+pasv_address=$site
+"           >>       /etc/vsftpd.conf
+useradd     -m       hhj         -d      /home/hhj/
+echo        "hhj:fengkuang"      |       chpasswd
 systemctl   enable    vsftpd
 systemctl   restart   vsftpd
 netstat     -plnt
