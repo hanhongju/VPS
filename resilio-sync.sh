fc8bf122b74df27ccf9ac210479ca214638562c4
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-02-26 16:30:18 +0800

    Update resilio-sync.sh

--- resilio-sync.sh
+++ resilio-sync.sh
@@ -1,4 +1,4 @@
-apt    -y    install    wget
+apt    -y    install    wget curl
 wget   -c    https://download-cdn.resilio.com/stable/debian/amd64/0/resilio-sync_3.0.2.1058-1_amd64.deb
 # curl       --socks5-hostname    127.0.0.1:8000     -O     https://download-cdn.resilio.com/stable/debian/amd64/0/resilio-sync_3.0.2.1058-1_amd64.deb
 dpkg   -i    resilio-sync_3.0.2.1058-1_amd64.deb
