f8224b65f6fde3f9dc0c432e6a15a46256d032f5
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-02-27 10:28:40 +0800

    Update resilio-sync.sh

--- resilio-sync.sh
+++ resilio-sync.sh
@@ -1,7 +1,7 @@
 apt    -y    install    wget curl
 wget   -c    https://download-cdn.resilio.com/2.7.3.1381/Debian/resilio-sync_2.7.3.1381-1_amd64.deb
 # curl -L    --socks5-hostname    127.0.0.1:8000     -O     https://download-cdn.resilio.com/2.7.3.1381/Debian/resilio-sync_2.7.3.1381-1_amd64.deb
-dpkg   -i    resilio-sync_2.7.3.1381-1_amd64.deb
+apt    -y    install    resilio-sync_2.7.3.1381-1_amd64.deb
 echo   '
 {"storage_path" : "/var/lib/resilio-sync/"
 ,"pid_file"     : "/var/run/resilio-sync/sync.pid"
