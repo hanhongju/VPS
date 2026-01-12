879a859afcd928772b2a9c16dbd198ccb85b84c9
hanhongju <63781648+hanhongju@users.noreply.github.com>
2026-01-13 00:08:42 +0800

    Update resilio-sync.sh

--- resilio-sync.sh
+++ resilio-sync.sh
@@ -1,7 +1,8 @@
+# Debian
 apt       -y    update
 apt       -y    install    curl net-tools
 curl      --location       --continue-at -            \
-          --remote-name    https://download-cdn.resilio.com/2.7.3.1381/Debian/resilio-sync_2.7.3.1381-1_amd64.deb      --socks5-hostname 127.0.0.1:4000
+          --remote-name    https://download-cdn.resilio.com/2.7.3.1381/Debian/resilio-sync_2.7.3.1381-1_amd64.deb
 apt       -y    install    ./resilio-sync_2.7.3.1381-1_amd64.deb
 mkdir     -p    /home/rslsync/
 chmod     -R    777   /home/rslsync/
@@ -21,5 +22,28 @@ netstat    -plnt
 
 
 
-# 私有云盘resilio-sync服务器搭建 @ Debian
-# 手机SYNC APP要将自启动和开机启动打开，自动休眠和省电策略关闭
+# CentOS 7
+yum       -y    update
+yum       -y    install    curl net-tools
+curl      --location       --continue-at -            \
+          --remote-name    https://download-cdn.resilio.com/stable/rpm/x86_64/0/resilio-sync-2.8.1.1390-1.x86_64.rpm
+yum       -y    install    ./resilio-sync-2.8.1.1390-1.x86_64.rpm
+mkdir     -p    /home/rslsync/
+chmod     -R    777   /home/rslsync/
+usermod   -aG   rslsync   hj
+echo   '
+{"storage_path" : "/var/lib/resilio-sync/"
+,"pid_file"     : "/var/run/resilio-sync/sync.pid"
+,"webui" : {"force_https" : false
+           ,"listen"      : "0.0.0.0:8888"
+           }
+}
+'          >         /etc/resilio-sync/config.json
+systemctl  enable    resilio-sync
+systemctl  restart   resilio-sync
+netstat    -plnt
+
+
+
+
+# 私有云盘resilio-sync服务器搭建
