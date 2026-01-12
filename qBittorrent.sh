f3c4a710246faf32b67b9823f79f6f61d4b183c6
hanhongju <63781648+hanhongju@users.noreply.github.com>
2026-01-13 00:24:19 +0800

    Update qBittorrent.sh

--- qBittorrent.sh
+++ qBittorrent.sh
@@ -44,14 +44,15 @@ netstat     -plnt
 
 
 # CentOS 7
-yum -y update
-yum -y install qbittorrent-nox net-tools
-systemctl stop firewalld
-systemctl disable firewalld
+yum           makecache
+yum     -y    install qbittorrent-nox net-tools
+systemctl     stop    firewalld
+systemctl     disable firewalld
 echo    '
-0 1 * * *     yum clean all
-0 2 * * *     yum makecache
-0 * * * *     pkill         tcrond
+0 1 * * *     yum       makecache
+0 2 * * *     yum  -y   update
+0 3 * * *     yum       clean    all
+0 * * * *     pkill     tcrond
 '       |     crontab
 echo    '
 [Unit]
